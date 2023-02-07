cbce6bf729d8816f5b780892c7b8dbc7e2103f55
Minhyunggi <alsvhtks@naver.com>
2023-02-07 15:45:18 +0900

    Interface

---
+++ src/chap_08/_02_Interface.java
@@ -0,0 +1,33 @@
+package chap_08;
+
+import chap_08.camera.FactoryCam;
+import chap_08.detector.AdvancedFireDetector;
+import chap_08.detector.Detectable;
+import chap_08.detector.FireDetector;
+import chap_08.repoter.NormalReporter;
+import chap_08.repoter.VideoReprter;
+
+public class _02_Interface {
+    public static void main(String[] args) {
+        // 인터페이스
+        NormalReporter normalReporter = new NormalReporter();
+        normalReporter.report();
+        VideoReprter videoReprter = new VideoReprter();
+        videoReprter.report();
+        System.out.println("--------------");
+
+        Detectable fireDetector = new FireDetector();
+        fireDetector.detect();
+        Detectable advancedFireDetector = new AdvancedFireDetector();
+        advancedFireDetector.detect();
+
+        System.out.println("--------------");
+
+        FactoryCam factoryCam = new FactoryCam();
+        factoryCam.setDetector(advancedFireDetector);
+        factoryCam.setReporter(videoReprter);
+
+        factoryCam.detect();
+        factoryCam.report();
+    }
+}
