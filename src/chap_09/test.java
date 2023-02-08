7d60bd9200828208aa366856c26a97a5de9f37f9
Minhyunggi <alsvhtks@naver.com>
2023-02-08 10:19:29 +0900

    GenericClass

---
+++ src/chap_09/test.java
@@ -0,0 +1,11 @@
+package chap_09;
+
+public class test {
+    public static void main(String[] args) {
+        int[] a = {1,2,3};
+        for (int array :
+                a) {
+            System.out.println(array);
+        }
+    }
+}
