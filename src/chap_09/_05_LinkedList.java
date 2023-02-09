188358786eeaf8a6f468df9042298cbc91c25182
Minhyunggi <alsvhtks@naver.com>
2023-02-09 10:22:32 +0900

    HashSet

--- src/chap_09/_05_LinkedList.java
+++ src/chap_09/_05_LinkedList.java
@@ -1,5 +1,6 @@
 package chap_09;
 
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class _05_LinkedList {
@@ -37,6 +38,56 @@ public class _05_LinkedList {
         list.add(1,"김영철");
         System.out.println("학생 추가 후 : " + list.get(1));
         System.out.println("학생 추가 후 : " + list.get(2));
+        System.out.println("---------------");
 
+        // 삭제
+        System.out.println("남은 학생 수(제외 전) : " + list.size());
+        list.remove(list.size()-1);
+        System.out.println("남은 학생 수(제외 후) : " + list.size());
+
+        // 처음 학생과 마지막 학생이 전학
+        System.out.println("남은 학생 수(전학 전) : " + list.size());
+        list.removeFirst(); // 처음데이터 삭제
+        list.removeLast(); // 마직막데이터 삭제
+        System.out.println("남은 학생 수(전학 후) : " + list.size());
+        for (String s : list) {
+            System.out.println(s);
+        }
+        System.out.println("---------------");
+
+        //수강권 양도
+        list.set(0, "이수근");
+        System.out.println(list.get(0));
+
+        //확인
+        System.out.println(list.indexOf("김종국"));
+        if (list.contains("김종국")) {
+            System.out.println("수강 신청 성공");
+        } else {
+            System.out.println("수강 신청 실패");
+        }
+        System.out.println("---------------");
+
+        // 전체 삭제
+        list.clear();
+        if (list.isEmpty()) {
+            System.out.println("학생 수 : " + list.size());
+            System.out.println("리스트가 비었습니다.");
+        }
+        System.out.println("---------------");
+
+        // 새로운 학기
+        list.add("유재석");
+        list.add("조세호");
+        list.add("김종국");
+        list.add("박명수");
+        list.add("강호동");
+        list.add("강호동");
+        list.add("강호동"); // List 에서는 중복 허용
+
+        Collections.sort(list); // 정렬
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
